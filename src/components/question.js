3ffeaf1a122ef59c6a38ab460e6c641299137f96
佐々木正男 <masaosasaki@MacBook-Pro.local>
2020-12-16 12:49:44 +0900

    Update question.js

--- src/components/question.js
+++ src/components/question.js
@@ -1,12 +1,17 @@
+
+// 参考サイト: https://ch.nicovideo.jp/mentalist/blomaga/ar1819322
 let Question = [
-  { id: 1, title: "受容", body: "ありのままを受け入れてもらう", importance: "" },
-  // { id: 2, title: "正確", body: "自分の意思や信念を正しく伝える",importance: "" },
-  // { id: 3, title: "達成", body: "なにか重要なことを達成する", importance: "" },
-  // { id: 4, title: "冒険", body: "新しくてワクワクする体験をする", importance: "" },
-  // { id: 5, title: "魅力", body: "身体的な魅力を保つ", importance: "" },
-  // { id: 6, title: "権威", body: "他者に対して責任を持って指導する", importance: "" },
-  // { id: 7, title: "自治", body: "人まかせにしないで自分で決める", importance: "" },
-  { id: 8, title: "美的", body: "身のまわりの美しいものを味わう", importance: "" }
+  { id: 1, title: "受容", body: "ありのままを受け入れてもらう" },
+  { id: 2, title: "正確", body: "自分の意思や信念を正しく伝える" },
+  { id: 3, title: "達成", body: "なにか重要なことを達成する" },
+  { id: 4, title: "冒険", body: "新しくてワクワクする体験をする" },
+  { id: 5, title: "魅力", body: "身体的な魅力を保つ" },
+  { id: 6, title: "権威", body: "他者に対して責任を持って指導する" },
+  { id: 7, title: "自治", body: "人まかせにしないで自分で決める" },
+  { id: 8, title: "美的", body: "身のまわりの美しいものを味わう" },
+  { id: 9, title: "庇護", body: "他社のめんどうをみる" },
+  { id: 10, title: "挑戦", body: "難しい仕事や問題に取り組む" },
+  { id: 11, title: "変化", body: "変化に富んだバラエティ豊かな人生を送る" },
 ]
 
 export default Question;
